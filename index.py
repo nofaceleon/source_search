60d12aa759402e37555cd231d7d406451b33b788
无脸 <229883447@qq.com>
2021-07-09 23:13:21 +0800

    improve

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ from ui.about import Ui_Dialog
 # 使用自定义信号去控制页面元素的修改
 class mySignal(QObject):
     btnChange = pyqtSignal(str)  # 自定义信号
+    statusBarChange = pyqtSignal(str)
 
 
 # 每一个窗口都是一个类文件
@@ -34,8 +35,10 @@ class main(Ui_MainWindow, QMainWindow):
     # 业务逻辑
     def logic(self):
         self.searchBtn.clicked.connect(self.search)  # 监听点击事件
-        self.ms.btnChange.connect(self.btn_text_change)  # 监听自定义信号
         self.actionversion.triggered.connect(self.show_about)
+
+        self.ms.btnChange.connect(self.btn_text_change)  # 监听自定义信号
+        self.ms.statusBarChange.connect(self.status_bar_text_change)
 
     def show_about(self):
         # 控制显示, 必须是全局变量才能监听, 否则会闪退
@@ -46,11 +49,19 @@ class main(Ui_MainWindow, QMainWindow):
     def btn_text_change(self, text):
         self.searchBtn.setText(text)
 
+    # 修改状态文字
+    def status_bar_text_change(self, text):
+        self.statusbar.showMessage(text)
+
     # 开始搜索
     def search(self):
         # 不在主线程中执行这个
         keyword = self.movieName.text()  # 获取关键字
+
+        # 判断搜索类型, 图书还是电影
+
         self.ms.btnChange.emit('搜索中')  # 发送自定义信号
+        self.ms.statusBarChange.emit(f"【{keyword}】 正在搜索中....")
 
         # 使用多线程来防止阻塞,影响页面渲染
         def t_run(key):
@@ -60,7 +71,8 @@ class main(Ui_MainWindow, QMainWindow):
             end = time.time()
             total_time = '%.2f' % (end - start)
             self.showResult(data)
-            self.statusbar.showMessage(f"【{key}】 搜索完成, 共找到{len(data)}条数据, 总耗时{total_time}s")
+            # self.statusbar.showMessage(f"【{key}】 搜索完成, 共找到{len(data)}条数据, 总耗时{total_time}s")
+            self.ms.statusBarChange.emit(f"【{key}】 搜索完成, 共找到 {len(data)} 条数据, 总耗时 {total_time}s")
             self.ms.btnChange.emit('开始搜索')  # 发送自定义信号
 
         task = Thread(target=t_run, args=(keyword,))  # 线程只有一个参数的时候也必须用元组的方式传递参数
@@ -81,7 +93,7 @@ class main(Ui_MainWindow, QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ui = main()
-    ui.show()
-    dialog = dialog_w() # 子窗口只能全局实例化
+    ui = main()  # 主窗口实例化
+    ui.show()  # 主窗口展示
+    dialog = dialog_w()  # 子窗口只能全局实例化
     sys.exit(app.exec_())
